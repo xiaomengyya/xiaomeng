7cc750281e8583f45744ef27550d10dc73be27c8
xiaomeng <rmeng@upei.ca>
2024-12-04 14:49:50 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/upei/project/Card/Card.java

--- src/main/java/upei/project/People/Person.java
+++ src/main/java/upei/project/People/Person.java
@@ -3,8 +3,11 @@ package upei.project.People;
 public class Person {
     private final String name;
     private final int HP;
+
     public Person(String name, int HP) {
         this.name = name;
         this.HP = HP;
     }
+
+
 }
