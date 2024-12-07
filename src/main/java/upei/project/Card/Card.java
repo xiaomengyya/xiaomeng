5813701fdf2fa9e1bc3b5ee97df29fd5d0eb4052
xiaohany <rgnyu99@gmail.com>
2024-12-07 11:35:17 -0400

    Card.java

--- src/main/java/upei/project/Card/Card.java
+++ src/main/java/upei/project/Card/Card.java
@@ -1,16 +1,38 @@
 package upei.project.Card;
+import upei.project.Controller.GameController;
 import upei.project.People.Person;
 
-public class Card {
-    private final int number;
+public abstract class Card {
+    private int number;
+    //private String type;
+    private String info;
+    private String name;
     private Person owner = null;
     private Person target = null;
-    public Card(int number) {
+
+    public Card(String name, String description, int number) {
+        this.name = name;
+        this.info = description;
         this.number = number;
     }
+
+    /*Getter*/
     public int getNumber() {
         return this.number;
     }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return this.info;
+    }
+
+
+
+    // Abstract method, card effects need to be implemented by subclasses
+    public abstract void activateEffect(Person self, Person opponent, GameController game);
+
+
     public void setOwner(Person player) {
         this.owner = player;
     }
